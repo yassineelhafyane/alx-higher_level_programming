c70661d94716bda5d038ef6fd2e00f45522ddf34
yassineelhafyane <yassineelhafyane99@gmail.com>
2023-10-25 00:15:02 +0200

    0x05 python

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
-
 def safe_print_division(a, b):
-    """
-    divides two integers and prints the result
-    catches divide by zero exception
-    """
     try:
-        res = a / b
-    except:
-        res = None
+        division = a / b
+    except (TypeError, ZeroDivisionError):
+        division = None
     finally:
-        print("Inside result: {}".format(res))
-    return res
+        print("Inside result: {}".format(division))
+    return (division)
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
 
-
 def list_division(my_list_1, my_list_2, list_length):
-    """
-    takes two lists and creates a new list with result of divison
-    operation
-
-    handles errors and prints them to stdout
-    """
-    i = 0
     new_list = []
-    result = 0
-    for i in range(0, list_length):
+    for element in range(0, list_length):
         try:
-            result = (my_list_1[i] / my_list_2[i])
+            division = my_list_1[element] / my_list_2[element]
         except TypeError:
-            result = 0
             print("wrong type")
+            division = 0
         except ZeroDivisionError:
-            result = 0
             print("division by 0")
+            division = 0
         except IndexError:
-            result = 0
             print("out of range")
+            division = 0
         finally:
-            new_list.append(result)
-    return new_list
+            new_list.append(division)
+    return (new_list)
